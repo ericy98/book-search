1de9371bcb96155f154b682582fcf598773b042b
Eric Yarbrough <eric.yarbrough@utexas.edu>
2020-10-17 21:10:21 -0500

    add in saved book & search

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+
+import { useQuery, useMutation } from '@apollo/react-hooks';
 import { QUERY_ME } from '../utils/queries';
 import { REMOVE_BOOK } from '../utils/mutations';
-import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
-import { useMutation, useQuery } from '@apollo/react-hooks';
+
+import Auth from '../utils/auth';
 
 const SavedBooks = () => {
-  // use this to determine if `useEffect()` hook needs to run again
   const { loading, data } = useQuery(QUERY_ME);
   const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
@@ -15,6 +16,7 @@ const SavedBooks = () => {
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
+    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -25,12 +27,14 @@ const SavedBooks = () => {
       await removeBook({
         variables: { bookId },
       });
+
+      // upon success, remove book's id from localStorage
       removeBookId(bookId);
-    } catch (e) {
-      console.error(e);
+    } catch (err) {
+      console.error(err);
     }
   };
-  // if data isn't here yet, say so
+
   if (loading) {
     return <h2>LOADING...</h2>;
   }
@@ -44,20 +48,26 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {userData.savedBooks.length
-            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
+          {userData.savedBooks?.length
+            ? `Viewing ${userData.savedBooks.length} saved ${
+                userData.savedBooks.length === 1 ? 'book' : 'books'
+              }:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {userData.savedBooks.map((book) => {
+          {userData.savedBooks?.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
-                {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
+                {book.image ? (
+                  <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' />
+                ) : null}
                 <Card.Body>
                   <Card.Title>{book.title}</Card.Title>
                   <p className='small'>Authors: {book.authors}</p>
                   <Card.Text>{book.description}</Card.Text>
-                  <Button className='btn-block btn-danger' onClick={() => handleDeleteBook(book.bookId)}>
+                  <Button
+                    className='btn-block btn-danger'
+                    onClick={() => handleDeleteBook(book.bookId)}>
                     Delete this Book!
                   </Button>
                   {error && <span className="ml-2">Something went wrong...</span>}
--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -27,15 +27,15 @@ export const ADD_USER = gql`
 export const SAVE_BOOK = gql`
     mutation saveBook($bookData: BookInput!) {
         saveBook(bookData: $bookData) {
-            _id
-            username
-            email
-            savedBooks {
+        _id
+        username
+        email
+        savedBooks {
                 bookId
                 authors
-                image  
-                title
+                image
                 description
+                title
                 link
             }
         }
